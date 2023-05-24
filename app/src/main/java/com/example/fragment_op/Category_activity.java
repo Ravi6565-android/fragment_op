6b948c2be688a108a83ad585f53b3edf4dcd6c16
ravi6565-android <ravichauhanis420@gmail.com>
2023-05-24 11:58:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragment_op/Category_activity.java
@@ -0,0 +1,14 @@
+package com.example.fragment_op;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Category_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
